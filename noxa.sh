b5b1d0542e6f2f66d5e39b0f56a7a9497237b115
psonytub-dot <psonytub@gmail.com>
2026-09-11 21:38:07 +0300

    Update noxa.sh

--- noxa.sh
+++ noxa.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-# Ekranı temizle
 clear
-
-# Neon Mavi Logo
 echo -e "\e[96m"
 echo " _   _  ___  __  __  _    "
 echo "| \ | |/ _ \ \ \/ / / \   "
@@ -15,18 +12,17 @@ echo -e "\e[0m"
 echo -e "\e[92m[+] Noxa Core Kurulum Protokolü Başlatıldı...\e[0m"
 sleep 1
 
-echo -e "\e[93m[~] Gerekli kütüphaneler yükleniyor (Pyrogram & TgCrypto)...\e[0m"
+echo -e "\e[93m[~] Gerekli kütüphaneler yükleniyor...\e[0m"
 pip install pyrogram tgcrypto > /dev/null 2>&1
 
-echo -e "\e[93m[~] Uzay modülleri için klasörler oluşturuluyor...\e[0m"
+echo -e "\e[93m[~] Klasörler oluşturuluyor...\e[0m"
 mkdir -p noxa_bot/plugins
 cd noxa_bot
 
 echo -e "\e[93m[~] Noxa Core dosyaları sunucudan çekiliyor...\e[0m"
-# Not: Aşağıdaki URL'yi, kendi noxa.py dosyanızı yüklediğiniz RAW linkiyle değiştirin
-curl -sL "https://raw.githubusercontent.com/SENIN_GITHUB_ADIN/Noxa-Bot/main/noxa.py" -o noxa.py
+# İşte anahtar kısım burası:
+curl -sL "https://raw.githubusercontent.com/psonytub-dot/noxauserbot/main/noxa.py" -o noxa.py
 
 echo -e "\e[92m[+] Kurulum Tamamlandı!\e[0m"
-echo -e "\e[96m[>] Botu başlatmak için şu komutları girin:\e[0m"
-echo -e "    cd noxa_bot"
+echo -e "\e[96m[>] Botu başlatmak için şu komutu girin:\e[0m"
 echo -e "    python noxa.py\n"
